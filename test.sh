071f52999733383165ba1247b84f501856c8319e
mkhellou <yoyomcawom@gmail.com>
2023-01-02 09:34:06 +0100

    deleted test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SERVERID=$(top -l 1 | grep server_bonus | awk '{print $1}')
-for i in {1..100000}
-do
-		./client_bonus $SERVERID  "📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗📗"
-done
